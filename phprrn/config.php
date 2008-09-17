90e103f9cb920313a0b6b391001e6cc3c0f82d48
alan.gustavo <alan.gustavo@a195533c-84df-11dd-82a6-aba1b78984bd>
2008-09-17 19:07:59 +0000

    Arquivo de configuracao automática. Usado para localizar rapidamente as classes da arquitetura, fazendo o include automatico das classes instanciadas.

---
+++ phprrn/config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * O que é o Arquivo. Ex:Arquivo de Configuração da Classe TConexao
+ * @author Alan Gustavo Santana Ribeiro <alan@prrn.mpf.gov.br> em 17/09/2008
+ * @version 1.0
+ * @package
+ */
+$CAMINHO 	= str_replace("config.php","",__FILE__);
+$DS			= DIRECTORY_SEPARATOR;
+function __autoload($classe)
+{
+	// Aqui armazeno o caminho de config.php (retirando o nome do arquivo).
+	// Retorna algo como C:\xampp\htdocs\Orion\asspa\gc\
+	$CAMINHO 	= str_replace("config.php","",__FILE__);
+	$DS			= DIRECTORY_SEPARATOR;
+	// Essa função procura a classe no caminho especificado.
+    if (file_exists("$CAMINHO$classe.class.php"))
+    {
+        include_once "$CAMINHO$classe.class.php";
+    }
+}
+?>
